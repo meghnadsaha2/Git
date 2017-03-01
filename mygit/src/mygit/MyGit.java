10f38d69012dba47ea5d8f1bcd3d2d1ed2cec207
meghnadsaha <meghnadsaha422@gmail.com>
2017-03-01 11:20:53 +0530

    old.

--- mygit/src/mygit/MyGit.java
+++ mygit/src/mygit/MyGit.java
@@ -4,6 +4,6 @@ public class MyGit {
 
 	public static void main(String args[]) {
 		System.out.println("Hello");
-		System.out.println("--------------------------");
+		System.out.println("-------------------------");
 	}
 }
